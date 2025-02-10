9d3e594969aba90fc259625b3503a694a0e23738
KaranSatyajitChauhan <120558948+KaranSatyajitChauhan@users.noreply.github.com>
2025-02-10 15:07:46 -0500

    APIs
    Created Apis

---
+++ Api/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const mySql = require("mysql2/promis");
+
+const api = express();
+const port = 3000;
+api.use(express.json());
+
+api.get("/",(req,res)=>{
+        res.status(200).json({ message: "Welcome to the API!",})
+});
+
+
+api.listen(port,()=>{
+    console.log("The server is running on port number"+ port);
+});
